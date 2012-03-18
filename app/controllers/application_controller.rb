d4392c98751f6fe54490f710b4b44429cb7e8d7f
kevin <kevin@kevinformatics.com>
2012-03-18 15:57:00 -0700

    Added autoreloading of lib dir in development so that developing parsers is easier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :reload_libs if Rails.env.development?
+  
   private
+    # http://hemju.com/2011/02/11/rails-3-quicktip-auto-reload-lib-folders-in-development-mode/
+    def reload_libs
+      Dir["#{Rails.root}/lib/**/*.rb"].each { |path| require_dependency path }
+    end
 
     def current_user
       @current_user ||= User.find_by_auth_token( cookies[:auth_token]) if cookies[:auth_token]
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Webapp
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     # config.autoload_paths += %W(#{config.root}/lib/**/)
+    config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"] 
     # config.autoload_paths += %W(#{config.root}/lib/scrapers/**/)
 
